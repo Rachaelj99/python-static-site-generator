202816df182222d279304765694e0f4d9419afbd
Rachael Jeon <rachaeljeon99@gmail.com>
2021-08-18 10:52:25 -0400

    COmpleted module

---
+++ ssg.py
@@ -0,0 +1,14 @@
+import typer
+
+from ssg.site import site
+
+#a function that captures command line arguments
+def main(source="content", dest="dist"):
+    config = {"source": source, "dest": dest}
+
+    #creates an instance of 'Site' class
+    #2 attributes/args = unpacked dictionary values with **
+    #chains a call to the build()
+    Site(**config).build()
+
+typer.run(main)
--- ssg/site.py
+++ ssg/site.py
@@ -1,1 +1,24 @@
 import Path from pathlib
+
+class Site:
+
+    #constructor that accepts 3 arugments
+    #instance attributes = prefixed with self
+    def __init__(self, source, dest):
+        #convert source to a Path object
+        self.source = Path(source)
+        self.dest = Path(dest)
+
+    def create_dir(self, path):
+        #contains the full path to the destination folder
+        directory = self.dest / path.relative_to(self.source)
+        #want 'directory' to be replaced if it exists
+        directory.mkdir(parents=True, exist_ok=True)
+
+    def build(self):
+        self.dest.mkdir(parents=True, exist_ok=True)
+        #iterate through the paths of self.source.rglob("*")
+        for path in self.source.rglob("*"):
+            #if current path is  a directory
+            if path.is_dir():
+                self.create_dir(path)
